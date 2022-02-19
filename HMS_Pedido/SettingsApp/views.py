1924394ad58fdc64c73f3807c77cad79450bf586
Bishal Raj Parajuli <hello76tortugaa@protonmail.com>
2022-02-19 19:38:24 +0545

    Purchase Category List with Search Created

--- HMS_Pedido/SettingsApp/views.py
+++ HMS_Pedido/SettingsApp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
-from SettingsApp.models import PurchaseItems
+from SettingsApp.models import PurchaseItemsCategory
 # Create your views here.
 
 def ListPurchaseCategory(request):
-    context = {
-        'objects': PurchaseItems.objects.all()
-    }
-    return render(request, 'Settings/Category/list-purchase-category.html', context)
+    if request.method == 'POST':
+        search = request.POST['list-search']
+        objects = PurchaseItemsCategory.objects.filter(category_name__contains=search)
+        context = {
+            'objects':objects
+        }
+        return render(request, 'Settings/Category/list-purchase-category.html', context)
+    else:
+        objects = PurchaseItemsCategory.objects.all()
+        context = {
+            'objects': objects
+        }
+        return render(request, 'Settings/Category/list-purchase-category.html', context)
