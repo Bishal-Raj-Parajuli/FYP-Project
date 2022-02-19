be2f6ecaf8c0e4102ed9d1da29a1f2d7d3b045ae
Bishal Raj Parajuli <hello76tortugaa@protonmail.com>
2022-02-19 15:44:00 +0545

    Creating User App

---
+++ HMS_Pedido/UserAuth/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    permanent_address = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=255)
+
+
+
+
+
+
